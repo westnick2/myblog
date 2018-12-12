c852f1da0362d7ddedfdb75a3cc005373192b8e6
westnick2 <vmetw@ukr.net>
2018-12-12 23:57:57 +0200

    added user model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
   def new
     @post = Post.new
@@ -20,18 +21,27 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update_attributes(post_params)
       redirect_to @post
     else
       render :edit
     end
   end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+
     private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
 
     def post_params
       params.require(:post).permit(:title, :summary, :body)
