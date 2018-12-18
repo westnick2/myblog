57721dae4bb3d6a2c330bbd44b4d5d57d4de5c4a
westnick2 <vmetw@ukr.net>
2018-12-18 13:29:29 +0200

    change text_field to text_area for post-body

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,52 +4,52 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  has_one :cover, as: :imageable, class_name: 'Image', dependent: :destroy
-  accepts_nested_attributes_for :cover
-
-  validates :username, uniqueness: true, presence: true
-  validates :email, uniqueness: true, presence: true, email: true
-  validates :password, length: { minimum: 6 }, presence: true
-
-  before_save :encrypt_password, if: -> { password.present? }
-
-  def self.authenticate(email, password)
-    user = find_by(email: email)
-
-    user if user&.correct_password?(password)
-  end
-
-  def correct_password?(password)
-    password_hash == BCrypt::Engine.hash_secret(password, password_salt)
-  end
-
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-               BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-  def User.new_token
-    SecureRandom.urlsafe_base64
-  end
-
-  def create_reset_digest
-    self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
-  end
-
-  def send_password_reset_email
-    UserMailer.password_reset(self).deliver_now
-  end
-
-  def password_reset_expired?
-    reset_sent_at < 2.hours.ago
-  end
-
-  private
-  def encrypt_password
-    self.password_salt = BCrypt::Engine.generate_salt
-    self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
-  end
+  # has_one :cover, as: :imageable, class_name: 'Image', dependent: :destroy
+  # accepts_nested_attributes_for :cover
+  #
+  # validates :username, uniqueness: true, presence: true
+  # validates :email, uniqueness: true, presence: true, email: true
+  # validates :password, length: { minimum: 6 }, presence: true
+  #
+  # before_save :encrypt_password, if: -> { password.present? }
+  #
+  # def self.authenticate(email, password)
+  #   user = find_by(email: email)
+  #
+  #   user if user&.correct_password?(password)
+  # end
+  #
+  # def correct_password?(password)
+  #   password_hash == BCrypt::Engine.hash_secret(password, password_salt)
+  # end
+  #
+  # def User.digest(string)
+  #   cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+  #              BCrypt::Engine.cost
+  #   BCrypt::Password.create(string, cost: cost)
+  # end
+  #
+  # def User.new_token
+  #   SecureRandom.urlsafe_base64
+  # end
+  #
+  # def create_reset_digest
+  #   self.reset_token = User.new_token
+  #   update_attribute(:reset_digest, User.digest(reset_token))
+  #   update_attribute(:reset_sent_at, Time.zone.now)
+  # end
+  #
+  # def send_password_reset_email
+  #   UserMailer.password_reset(self).deliver_now
+  # end
+  #
+  # def password_reset_expired?
+  #   reset_sent_at < 2.hours.ago
+  # end
+  #
+  # private
+  # def encrypt_password
+  #   self.password_salt = BCrypt::Engine.generate_salt
+  #   self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  # end
 end
